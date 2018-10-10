a33e703a70a69c22c81a8a5717f917f5f0c1e49e
Lauren Schroeder <schroederle@schroederle-mp.local>
2018-10-10 15:45:24 -0500

    all files with lead added

---
+++ public/js/addon.js
@@ -0,0 +1,26 @@
+/* add-on script */
+/* app script */
+// Myapp functionality
+$(function() {
+// Call REST API via the iframe
+// Bridge functionality
+// JiraActivity is registered by an external script that was included
+AP.require(['request', 'JiraActivity'], function(request, JiraActivity) {
+    request({
+        url: '/rest/api/2/project?expand=lead',
+        success: function(response) {
+            // Convert the string response to JSON
+            response = JSON.parse(response);
+
+            // Call your helper function to build the
+            // table, now that you have the data
+            JiraActivity.buildProjectTable(response, ".projects");
+        },
+        error: function(response) {
+            console.log("Error loading API (" + uri + ")");
+            console.log(arguments);
+        },
+        contentType: "application/json"
+    });
+});
+});
